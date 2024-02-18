9131334aba093df24f22b30df767431849ee61b2
GombosBence <gombosbence08@gmail.com>
2024-02-18 17:48:20 +0100

    Widget and WidgetController classes

---
+++ Source/UE5_ARPG_Project/Public/UI/WidgetController/BaseWidgetController.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BaseWidgetController.generated.h"
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+/**
+ * 
+ */
+UCLASS()
+class UE5_ARPG_PROJECT_API UBaseWidgetController : public UObject
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
+	TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
+	TObjectPtr<UAttributeSet> AttributeSet;
+};
