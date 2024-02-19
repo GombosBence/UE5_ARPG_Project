037f5526ebf9bf130fca78a77eade2a08863c636
GombosBence <gombosbence08@gmail.com>
2024-02-19 11:16:48 +0100

    OverlayWidgetController

---
+++ Source/UE5_ARPG_Project/Public/UI/WidgetController/OverlayWidgetController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/BaseWidgetController.h"
+#include "OverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_ARPG_PROJECT_API UOverlayWidgetController : public UBaseWidgetController
+{
+	GENERATED_BODY()
+	
+};
